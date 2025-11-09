aa8958ac3a43e18e1e67c6cac0447755b3ad7ca3
Md Eftequarul Alam <efthaqur@gmail.com>
2025-11-09 21:41:36 +0600

    Add: TopRated Component

---
+++ src/Components/TopRated.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopRated = () => {
+    return (
+        <div>
+            <p className="text-center m-10 border-2 border-blue-700">Top rated Movies</p>
+        </div>
+    );
+};
+
+export default TopRated;

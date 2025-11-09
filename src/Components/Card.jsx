a8e2b69605143cb464f8d85f498adb17c1b12144
Md Eftequarul Alam <efthaqur@gmail.com>
2025-11-09 21:33:23 +0600

    Add: Card Component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <p className="text-center m-10 border-2 border-amber-500">About this Platform</p>
+        </div>
+    );
+};
+
+export default Card;

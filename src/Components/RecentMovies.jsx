07193f88fdbc094ecdb10658c438c4184ced88a2
Md Eftequarul Alam <efthaqur@gmail.com>
2025-11-09 21:34:35 +0600

    Add: Recent Movies Component

---
+++ src/Components/RecentMovies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecentMovies = () => {
+    return (
+        <div>
+            <p className="text-center m-10 border-2 border-b-emerald-700">About this Recent Movies</p>
+        </div>
+    );
+};
+
+export default RecentMovies;

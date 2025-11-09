cb3759e9f1addfdc953246f5f090e91c8f542350
Efthaqur Alam <efthaqur@gmail.com>
2025-11-09 14:26:38 +0600

    Test: Firebase ready, Add: Favicon in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
 
+
+
 function App() {
   
   return (
     <>
       <Navbar></Navbar>
       This is APP Page!
+
       <Footer></Footer>
     </>
   )

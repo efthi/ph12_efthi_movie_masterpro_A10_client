896917bcf68add732e7a029b7407eb030da03d9e
Md Eftequarul Alam <efthaqur@gmail.com>
2025-11-09 21:35:43 +0600

    Add: Slider Component

---
+++ src/Components/Slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Slider = () => {
+    return (
+        <div>
+            <p className="text-center m-10 border-2 border-[#800fff]">Slider</p>
+        </div>
+    );
+};
+
+export default Slider;

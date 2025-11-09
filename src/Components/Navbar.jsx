69057a9dbec81f03252e8e1b74e98ce6cea83674
Efthaqur Alam <efthaqur@gmail.com>
2025-11-09 16:13:45 +0600

    Fix: Navmenu, Add: toggle button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,56 @@
 import React from 'react';
 
 
+
 const defaultPropic ='https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=';
 
 const Navbar = () => {
     return (
         <>
         <div className="navbar bg-base-100 shadow-sm">
+            <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li><a>Item 1</a></li>
+                <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                </ul>
+                </li>
+                <li><a>Item 3</a></li>
+            </ul>
+            </div>
             <div className="navbar-start">
                 <span ><img className="max-h-[100] max-w-[200] shadow rounded-4xl" src="../../public/assets/logo/logo.png" alt="" /></span>
             </div>
+
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                <li><a>Item 1</a></li>
+                <li>
+                    <details>
+                    <summary>Parent</summary>
+                    <ul className="p-2">
+                        <li><a>Submenu 1</a></li>
+                        <li><a>Submenu 2</a></li>
+                    </ul>
+                    </details>
+                </li>
+                <li><a>Item 3</a></li>
+                </ul>
+            </div>           
             {/* Login button and Profile menu section starts here */}
             <div className="navbar-end gap-2">
-                <a className="btn">Button</a>
+                 {/* toogle button */}
+            <input type="checkbox"
+            className="toggle border-indigo-600 bg-indigo-500 checked:border-orange-500 checked:bg-orange-400 checked:text-orange-800"/>
+                <a className="btn btn-xs md:btn-md">Button</a>
                 <div class="dropdown dropdown-end">
                     <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
                         <div class="w-10 rounded-full">

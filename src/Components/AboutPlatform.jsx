9c6e5fa5b31a59f5ce9b0eff0507b03b63e22b09
Md Eftequarul Alam <efthaqur@gmail.com>
2025-11-09 21:32:44 +0600

    Add: About Platform Component

---
+++ src/Components/AboutPlatform.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPlatform = () => {
+    return (
+        <div>
+            <p className="text-center m-10 border-2 border-amber-500">About this Platform</p>
+        </div>
+    );
+};
+
+export default AboutPlatform;

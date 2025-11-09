d483f56f1ed8beb397b6e7c29d740263085fbff5
Efthaqur Alam <efthaqur@gmail.com>
2025-11-09 14:48:55 +0600

    Update: DaisyUI Theme added, light: fantansy, dark:business

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <>
-          <div className="navbar shadow-sm bg-primary text-primary-content">
+          <div className="navbar shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                 <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">

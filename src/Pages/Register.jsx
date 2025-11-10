f15cea891875f571a473c84511a354e57be3e580
Md Eftequarul Alam <efthaqur@gmail.com>
2025-11-10 22:25:10 +0600

    Ready: Login and Register Design

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Mail, SquareAsterisk, Image, Eye, EyeOff } from 'lucide-react';
+import { Link, NavLink } from 'react-router';
 
 const Register = () => {
 //Page Title
@@ -6,10 +8,67 @@ const title = useEffect(()=>{
     document.title = 'Register | Movie Master Pro '
   }, []);
 
+//
+const [showpass, setShwopass] = useState(false);
+
+//show/hide password toggle এর ফাংশন এখানে
+const showHidePass = (e) => {
+    e.preventDefault();
+   // console.log(showpass);
+    setShwopass(!showpass);
+};
+
+//handleSignUp
+const handleSignUp =(e) => {
+    e.preventDefault();
+}
+
+
     return (
-        <div>
-            <p className="text-center m-10 border-2 border-teal-700">Register</p>
+        <>
+        <div className='flex justify-center m-10'>
+        <div className='shadow-indigo-600 shadow-2xl'>
+            <form onSubmit=''>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                    <p className='font-bold text-xl text-center'>To Join, Register  </p>
+                    {/* Email section */}
+                    <label className="label">Email</label>
+                        <label className='input'><Mail color="#ff0000" />
+                            <input type="email" className="input input-primary" name="email" placeholder="Email" />
+                    </label>
+                    {/* Password Section */}
+                    <div className=''> 
+                    {/* আগের প্রজেক্টে position সেট করে নিচের password hide/show বাটনকে পজিশনিং করা হয়েছিল, কিন্তু daisyUI এর <label> এর কারণে আর লাগে নাই */}
+                    <label className="label">Password</label>
+                    <label className="input">
+                        <SquareAsterisk color="#ff0000" />
+                        <input type={ showpass ? 'text' : 'password'} className="input" name="password" placeholder="Password" />
+                        <button onClick={showHidePass}  className="btn btn-xs bg-transparent">
+                            {showpass ? (<Eye color="#ff0000" />) : (<EyeOff color="#ff0000" />)}
+                        </button>
+                    </label>
+                    </div>
+                    {/* Image input field */}
+                    <label className="label">Image</label>
+                    <label className="input">
+                        <Image color="#ff0000" />
+                        <input type="text" className="input" name='imageurl' placeholder="Place your image link here" />
+                    </label>
+                    <button className="btn btn-accent mt-4">Register</button>
+                    <p><span className='text-error text-sm font-bold underline'>Already have an account?</span>
+                    <NavLink to='/login'><span className='text-success font-extrabold text-sm'> Login!</span></NavLink>
+                    </p>
+                    <div className="divider">OR</div>
+                    {/* Google */}
+                    <button  className="btn bg-white text-black border-[#e5e5e5]">
+                        <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                        Login with Google
+                    </button>
+                </fieldset>
+            </form> 
+        </div> 
         </div>
+        </>
     );
 };
 

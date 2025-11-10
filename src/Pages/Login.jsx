cb3c49f9d262fddeabade73f2598ee05394e74a8
Efthaqur Alam <efthaqur@gmail.com>
2025-11-10 16:12:42 +0600

    Login Page scratch ready

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -9,6 +9,54 @@ const Login = () => {
     return (
         <div>
             <p className="text-center m-10 border-2 border-yellow-700">Login Page</p>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend">Login</legend>
+
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+                <label className="input">
+    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        strokeWidth="2.5"
+        fill="none"
+        stroke="currentColor"
+        >
+        <circle cx="11" cy="11" r="8"></circle>
+        <path d="m21 21-4.3-4.3"></path>
+        </g>
+    </svg>
+    <input type="search" className="grow" placeholder="Search" />
+    <kbd className="kbd kbd-sm">⌘</kbd>
+    <kbd className="kbd kbd-sm">K</kbd>
+    </label>
+    <label className="input">
+    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        strokeWidth="2.5"
+        fill="none"
+        stroke="currentColor"
+        >
+        <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+        <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+        </g>
+    </svg>
+    <input type="text" className="grow" placeholder="index.php" />
+    </label>
+    <label className="input">
+    Path
+    <input type="text" className="grow" placeholder="src/app/" />
+    <span className="badge badge-neutral badge-xs">Optional</span>
+    </label>
         </div>
     );
 };

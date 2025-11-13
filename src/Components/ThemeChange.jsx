24f092a987b011f5aa91ae4ee467188e97eef12d
Efthaqur Alam <efthaqur@gmail.com>
2025-11-13 11:52:44 +0600

    Feature: Theme saved on reloading

--- src/Components/ThemeChange.jsx
+++ src/Components/ThemeChange.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ThemeChange = () => {
+
+const [theme, setTheme] = useState('light');
+
+useEffect(()=> {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    setTheme(savedTheme);
+    document.documentElement.setAttribute('data-theme', savedTheme);
+}, []);
+
+const handleToggle = (e) => {
+    const isChecked = e.target.checked;
+    const newTheme = isChecked ? 'business' : 'light;';
+    setTheme(newTheme);
+    localStorage.setItem('theme',newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+}
+
     return (
         <>
         <label className="toggle text-base-content">
-        <input type="checkbox" value="business" className="theme-controller" />
+        <input type="checkbox" className="theme-controller" value={theme} onChange={handleToggle} checked={theme === 'business'}  />
 
         <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
 
